d3e3042f8cb8bca3838934d9e72c5001b54c50bb
Caro Evans <caroline.w.evans@gmail.com>
2018-04-30 16:55:34 +1000

    scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :transactions
+  resources :locations
+  resources :stores
+  resources :categories
+  resources :products
+  resources :users
+  root to: "products#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
