ebf8061578635173b6ef9cd80e5cb502921c68f1
Caro Evans <caroline.w.evans@gmail.com>
2018-05-07 14:05:06 +1000

    fixing stores controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -26,11 +26,9 @@ class StoresController < ApplicationController
   def create
     @store = Store.new(store_params)
     @store.user_id = current_user.id
-
     respond_to do |format|
       if @store.save
         current_user.add_role(:owner)
-
         format.html { redirect_to @store, notice: 'Store was successfully created.' }
         format.json { render :show, status: :created, location: @store }
       else
@@ -72,6 +70,6 @@ class StoresController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def store_params
-      params.require(:store).permit(:name)
+      params.require(:store, :name).permit(:name)
     end
 end
