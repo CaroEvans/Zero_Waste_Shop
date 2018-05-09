4b5ef0bc5815c83d3576777c8d61a907a1f1b7bd
Caro Evans <caroline.w.evans@gmail.com>
2018-05-09 13:28:08 +1000

    seeting up dev environment for emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+
+config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    port: 587,
+    domain:"https://evening-wave-82509.herokuapp.com/",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    enable_starttls_auto: true
+}
+
 end
