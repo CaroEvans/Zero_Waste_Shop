1dfc82919e0728cab3a27235d69c90020cac837f
Caro Evans <caroline.w.evans@gmail.com>
2018-05-09 13:21:33 +1000

    geocoder done

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,8 @@
 class Location < ApplicationRecord
-
   validates :city, :state, :country, :post_code, presence: true
-
+  geocoded_by :full_street_address
+  after_validation :geocode
   def full_address
     "#{city}, #{state}, #{country}, #{post_code}"
   end
-
 end
