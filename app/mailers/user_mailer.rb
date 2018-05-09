f7aa0c32fe9609f4970a1fb28d5e12716b7791d3
Caro Evans <caroline.w.evans@gmail.com>
2018-05-09 21:22:29 +1000

    allowed for user without profile in profile index view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-  default from: 'caro.evans.dev@gmail.com'
 
   def send_signup_email(user)
     @user = user
-    @url = 'example.com'
     mail(:to => @user.email, :subject => 'Thanks for signing up')
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,6 +5,7 @@ class Location < ApplicationRecord
   validates :city, :state, :country, :post_code, presence: true
   geocoded_by :full_street_address
   after_validation :geocode
+  
   def full_address
     "#{city}, #{state}, #{country}, #{post_code}"
   end
